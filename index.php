7c89ccfc10fcb734bf8ec442a636b00cf748f2a8
akash <akasky70@gmail.com>
2021-10-02 13:00:51 +0545

    Setup site

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Shine Revive</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <header class="mt-3">
+    <div class="container">
+      <nav class="d-flex justify-content-between">
+        <div class="pt-2">
+          <strong class="lead">Logo</strong>
+        </div>
+        <ul class="navbar m-0">
+          <li class="nav-item">Home</li>
+          <li class="nav-item">Service</li>
+          <li class="nav-item">Contact</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <section>
+    <div class="container">
+      <div class="d-flex">
+        <div class="col-md-6 py-5 my-5 px-0">
+          <h1 class="bold">Welcome to <strong>ShineRevive!</strong></h1>
+          <p>We provide quality cleaning services in the city.</p>
+          <button class="btn btn-sm btn-outline-primary">Contact us
+            <i class="icon ion-ios-arrow-down ml-1"></i>
+          </button>
+        </div>
+        <div class="col-md-6"></div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="d-flex">
+        <h3>Over Service</h3>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="d-flex">
+        <h3>Portfolio</h3>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="d-flex">
+        <h3>Contact Us</h3>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <div class="d-flex">
+        <h3>Footer</h3>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
