30425995c5f4970f8bac53bdbc1d0042cd324725
akash <akasky70@gmail.com>
2021-10-03 00:05:44 +0545

    Add all the homepage sections

--- index.php
+++ index.php
@@ -6,68 +6,267 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Shine Revive</title>
+  <link rel="shortcut icon" href="./img/favicon.ico" />
   <link rel="stylesheet" href="./css/style.css">
+
 </head>
 
 <body>
-  <header class="mt-3">
+  <header class="m-3">
     <div class="container">
       <nav class="d-flex justify-content-between">
-        <div class="pt-2">
-          <strong class="lead">Logo</strong>
+        <div class="logo">
+          <img src="./img/logo.png" />
         </div>
-        <ul class="navbar m-0">
-          <li class="nav-item">Home</li>
-          <li class="nav-item">Service</li>
-          <li class="nav-item">Contact</li>
+        <ul class="navbar m-0 p-0">
+          <li class="nav-item">
+            <a href="/" class="active">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Service</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Contact</a>
+          </li>
         </ul>
       </nav>
     </div>
   </header>
 
-  <section>
-    <div class="container">
-      <div class="d-flex">
+  <section class="intro">
+    <div class="container py-5">
+      <div class="d-flex py-4">
         <div class="col-md-6 py-5 my-5 px-0">
-          <h1 class="bold">Welcome to <strong>ShineRevive!</strong></h1>
-          <p>We provide quality cleaning services in the city.</p>
-          <button class="btn btn-sm btn-outline-primary">Contact us
+          <h1 class="gradient-title">Reshine your property with ShineRevive!</strong></h1>
+          <p class="m-0">We provide quality cleaning services in the city.</p>
+          <p>Refer and get extra
+            <span class="h4 bold">10% off</span> on your next service.
+          </p>
+          <!-- 
+          <div class="d-flex">
+            <div class="alert alert-warning d-inline">
+              <i class="icon ion-md-pricetag mr-2"></i> REFERRRAL OFFER
+            </div>
+          </div> -->
+
+          <a href="#contact-us" class="btn btn-sm btn-outline-primary mt-3">Contact us
             <i class="icon ion-ios-arrow-down ml-1"></i>
-          </button>
+          </a>
         </div>
-        <div class="col-md-6"></div>
+        <div class="col-md-6">
+          <img src="./img/cleaning-tools.png" />
+        </div>
       </div>
     </div>
   </section>
 
   <section>
     <div class="container">
-      <div class="d-flex">
-        <h3>Over Service</h3>
+      <div class="">
+        <h1 class="bold gradient-title">Our Services</h1>
+      </div>
+      <div class="d-flex my-5">
+        <div class="col-md-4">
+          <div class="service-icon">
+            <i class="icon ion-md-trash h3 text-center d-block pt-2"></i>
+          </div>
+          <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis veritatis
+            itaque vel eos labore
+            praesentium, velit earum dolor nam architecto cupiditate! Ab reiciendis quo similique eligendi maiores sint
+            laudantium.</p>
+        </div>
+        <div class="col-md-4">
+          <div class="service-icon">
+            <i class="icon ion-md-trash h3 text-center d-block pt-2"></i>
+          </div>
+          <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis veritatis
+            itaque vel eos labore
+            praesentium, velit earum dolor nam architecto cupiditate! Ab reiciendis quo similique eligendi maiores sint
+            laudantium.</p>
+        </div>
+        <div class="col-md-4">
+          <div class="service-icon">
+            <i class="icon ion-md-trash h3 text-center d-block pt-2"></i>
+          </div>
+          <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis veritatis
+            itaque vel eos labore
+            praesentium, velit earum dolor nam architecto cupiditate! Ab reiciendis quo similique eligendi maiores sint
+            laudantium.</p>
+        </div>
       </div>
     </div>
   </section>
 
   <section>
     <div class="container">
-      <div class="d-flex">
-        <h3>Portfolio</h3>
+      <div class="">
+        <h1 class="bold gradient-title">Portfolio</h1>
+      </div>
+
+      <div class="d-flex row m-0 my-5">
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
+        <div class="col-md-3 p-1">
+          <div style="background:#e0e0e0; width:100%; height: 200px;"></div>
+        </div>
       </div>
     </div>
   </section>
 
-  <section>
+  <section id="contact-us" class="py-5">
     <div class="container">
-      <div class="d-flex">
-        <h3>Contact Us</h3>
+      <div class="">
+        <h1 class="bold gradient-title">Contact Us</h1>
+      </div>
+
+      <div class="d-flex py-2">
+        <div class="col-md-6 p-0 mt-5 pt-5">
+          <div class="d-flex w-100">We offer the following service. Contact us to reshine your property</div>
+
+          <div class="col-md-12 my-3">
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Rug Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Gutter Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Carpet Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Curtain Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Window Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Upholstery Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              End of Tenancy Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Patio and Driveway Cleaning
+            </div>
+            <div class="offer">
+              <i class="icon ion-md-checkmark-circle-outline mr-2"></i>
+              Wood Floor Sanding and Sealing
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-6 p-0">
+          <form class="col-md-12 p-0">
+            <div class="form-group">
+              <div class="col-md-12">
+                <label for="" class="mb-0">Email address</label>
+                <input type="text" class="form-control" placeholder="Enter email">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12">
+                <label for="" class="mb-0">Phone number</label>
+                <input type="text" class="form-control" placeholder="Enter phone number">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12">
+                <label for="" class="mb-0">Select Service</label>
+                <select class="form-control">
+                  <option>Select Service</option>
+                  <option>Service 1</option>
+                  <option>Service 2</option>
+                  <option>Service 3</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12">
+                <label for="" class="mb-0">Message</label>
+                <textarea class="form-control" id="" rows="4"></textarea>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12">
+                <button type="submit" class="btn btn-primary w-100">Submit</button>
+              </div>
+            </div>
+
+          </form>
+        </div>
       </div>
     </div>
   </section>
 
   <footer>
     <div class="container">
-      <div class="d-flex">
-        <h3>Footer</h3>
+      <div class="d-flex footer">
+        <div class="col-md-12 mt-5">
+          <div class="d-flex justify-content-center">
+            <div class="social-media">
+              <a href="#" class="social-media-icon">
+                <i class="icon h5 d-block mt-2 text-white ion-logo-instagram"></i>
+              </a>
+
+              <a href="#" class="social-media-icon">
+                <i class="icon h5 d-block mt-2 text-white ion-logo-facebook"></i>
+              </a>
+
+              <a href="#" class="social-media-icon">
+                <i class="icon h5 d-block mt-2 text-white ion-logo-whatsapp"></i>
+              </a>
+
+              <a href="#" class="social-media-icon">
+                <i class="icon h5 d-block mt-2 text-white ion-md-mail"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="d-flex justify-content-center mt-4">
+            <a href="#" class="mr-4">Terms of service</a>
+            <a href="#">Privacy policy</a>
+          </div>
+
+          <p class="text-center">
+            <a href='https://www.freepik.com/vectors/house'>House vector created by macrovector -
+              www.freepik.com</a>
+          </p>
+          <p class="text-center text-muted mb-5">
+            2021 &copy; All Rights Reserved
+          </p>
+        </div>
       </div>
     </div>
   </footer>
