e2c79fa1f1a7c1b9d648ba104d260818598a00b1
akash <akasky70@gmail.com>
2021-10-09 23:16:38 +0545

    Add document title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Shine Revive</title>
+  <title>Shine Revive | Reshine your property with ShineRevive!</title>
   <link rel="shortcut icon" href="./img/favicon.ico" />
   <link rel="stylesheet" href="./css/style.css">
 
