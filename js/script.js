f504b013bfa9f24303445e5d67121a5e37c541d3
akash <akasky70@gmail.com>
2021-10-10 14:24:42 +0545

    refactor js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  console.log(rect.top, el.id);
-
-  return (
-    rect.top >= -500 &&
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-  );
-}
-
 const body = select('#body');
 const home = select('#home');
 const service = select('#service');
@@ -18,45 +8,74 @@ const homeNav = select('#home-nav');
 const serviceNav = select('#service-nav');
 const contactNav = select('#contact-nav');
 
+const THEME = { DARK: 'dark', LIGHT: 'light' };
+const MENU = { HOME: 'home', SERVICE: 'service', CONTACT: 'contact' };
+
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= -500 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+function switchTheme(theme) {
+  if (THEME.DARK === theme) {
+    body.addClass('dark-bg');
+    body.removeClass('light-bg');
+
+    return;
+  }
+
+  body.addClass('light-bg');
+  body.removeClass('dark-bg');
+}
+
+function switchMenu(menu) {
+  homeNav.removeClass('active');
+  serviceNav.removeClass('active');
+  contactNav.removeClass('active');
+
+  if (MENU.HOME === menu) {
+    return homeNav.addClass('active');
+  }
+
+  if (MENU.SERVICE === menu) {
+    return serviceNav.addClass('active');
+  }
+
+  if (MENU.CONTACT === menu) {
+    return contactNav.addClass('active');
+  }
+}
+
 document.addEventListener(
   'scroll',
   function () {
     if (isInViewport(home)) {
-      body.addClass('dark-bg');
-      body.removeClass('light-bg');
-
-      homeNav.addClass('active');
-      serviceNav.removeClass('active');
-      contactNav.removeClass('active');
+      switchMenu(MENU.HOME);
+      switchTheme(THEME.DARK);
 
       return;
     }
 
     if (isInViewport(service)) {
-      body.addClass('light-bg');
-      body.removeClass('dark-bg');
-
-      serviceNav.addClass('active');
-      homeNav.removeClass('active');
-      contactNav.removeClass('active');
+      switchMenu(MENU.SERVICE);
+      switchTheme(THEME.LIGHT);
 
       return;
     }
 
     if (isInViewport(gallery)) {
-      body.addClass('dark-bg');
-      body.removeClass('light-bg');
+      switchTheme(THEME.DARK);
 
       return;
     }
 
     if (isInViewport(contactUs)) {
-      body.addClass('light-bg');
-      body.removeClass('dark-bg');
-
-      contactNav.addClass('active');
-      homeNav.removeClass('active');
-      serviceNav.removeClass('active');
+      switchMenu(MENU.CONTACT);
+      switchTheme(THEME.LIGHT);
 
       return;
     }
